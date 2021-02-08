f0f578f68446e3d995104a3428edddcc23afbc0f
Evgeniy_Er <5evgen_e@mail.ru>
2021-02-08 17:49:16 +0300

    Update Main.kt
    Added "create" and "status" commands and their processing

--- Main.kt
+++ Main.kt
@@ -14,9 +14,19 @@ class Car(val registrationNumber: String = "", val color: String = "")
 
 object ParkingLot {
 
+    // Изначально паркова без мест
+    var places: Array<Car?> = emptyArray()
+
+    fun create(qtyPlaces: Int) {
+        places = Array(qtyPlaces) { i -> null }
+        println("Created a parking lot with $qtyPlaces spots.")
+    }
+
     enum class Commands {
+        CREATE,
         PARK,
         LEAVE,
+        STATUS,
         EXIT,
         NULL;
 
@@ -30,39 +40,65 @@ object ParkingLot {
         }
     }
 
-    // Изначально паркова на 20 мест пустая
-    val places: Array<Car?> = Array(20) { i -> null }
-
     fun commandProcessing(userCommand: List<String>): Commands {
         val command = Commands.getByName(userCommand[0])
-        when (command) {
-            Commands.PARK -> {
-                val car = Car(userCommand[1], userCommand[2])
-                var isCarParked = false
-                for (i in places.indices) {
-                    if (places[i] == null) {
-                        places[i] = car
-                        println("${car.color} car parked in spot ${i + 1}.")
-                        isCarParked = true
-                        break
+        if(command == Commands.CREATE){
+            val qtyPlaces = userCommand[1].toInt()
+            create(qtyPlaces)
+        } else {
+            when (command) {
+                Commands.PARK -> {
+                    if (places.isEmpty()) {
+                        println("Sorry, a parking lot has not been created.")
+                        return Commands.PARK
+                    }
+                    val car = Car(userCommand[1], userCommand[2])
+                    var isCarParked = false
+                    for (i in places.indices) {
+                        if (places[i] == null) {
+                            places[i] = car
+                            println("${car.color} car parked in spot ${i + 1}.")
+                            isCarParked = true
+                            break
+                        }
+                    }
+                    if (!isCarParked) {
+                        println("Sorry, the parking lot is full.")
                     }
                 }
-                if (!isCarParked) {
-                    println("Sorry, the parking lot is full.")
+                Commands.LEAVE -> {
+                    if (places.isEmpty()) {
+                        println("Sorry, a parking lot has not been created.")
+                        return Commands.LEAVE
+                    }
+                    val numPlace = userCommand[1].toInt()
+                    if (places.size > numPlace - 1) {
+                        if (places[numPlace - 1] != null) {
+                            places[numPlace - 1] = null
+                            println("Spot $numPlace is free.")
+                        } else {
+                            println("There is no car in spot $numPlace.")
+                        }
+                    }
                 }
-            }
-            Commands.LEAVE -> {
-                val numPlace = userCommand[1].toInt()
-                if (places.size > numPlace - 1) {
-                    if (places[numPlace - 1] != null) {
-                        places[numPlace - 1] = null
-                        println("Spot $numPlace is free.")
-                    } else {
-                        println("There is no car in spot $numPlace.")
+                Commands.STATUS -> {
+                    if (places.isEmpty()) {
+                        println("Sorry, a parking lot has not been created.")
+                        return Commands.STATUS
+                    }
+                    var isEmptyParking = true
+                    for (i in places.indices) {
+                        if (places[i] != null) {
+                            println("${i + 1} ${places[i]!!.registrationNumber} ${places[i]!!.color}")
+                            isEmptyParking = false
+                        }
+                    }
+                    if (isEmptyParking) {
+                        println("Parking lot is empty.")
                     }
                 }
             }
         }
-        return command
+        return command // EXIT
     }
 }
